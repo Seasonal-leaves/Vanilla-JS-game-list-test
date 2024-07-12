e2340f9beca91ca1286216e04d37e91cf751caa0
Rituparno Sen <ritam_sen@ymail.com>
2024-07-12 13:18:44 +0530

    rebuild the basic game list app with vue

--- gs-02-starting-vue/app.js
+++ gs-02-starting-vue/app.js
@@ -1,1 +1,15 @@
-Vue.createapp()
+Vue.createApp({
+    data(){
+        return {
+            games:[],
+            enteredGame:''
+        }
+    }
+    methods:{
+        addGame(){
+            this.games.push(this.enteredGame)
+        }
+
+    }
+}).mount('#app'); //Vue as onject is now available thanks to the import in index.html. createApp() takes a javascript onject where we configure the vue app
+
