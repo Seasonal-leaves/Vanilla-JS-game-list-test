a0b4c340079a54b211a4e3ccc20196290f1252f6
Rituparno Sen <ritam_sen@ymail.com>
2024-07-13 00:34:49 +0530

    Added a method  to make the site a little more dynamic

--- creating and connecting vue app instances/app.js
+++ creating and connecting vue app instances/app.js
@@ -5,6 +5,16 @@ const app = Vue.createApp(
         addedGame: 'Finsih the game and add it', //inside the return object we can add any key value pair as we like
         intervuelink: 'https://joyful-speculoos-f3dc31.netlify.app/'
     }
+        },
+        methods:{ //methods is also a reserved word like data. Here it should always store functions. Remember it is already a property that stores functions so put ':' in stead of '()'
+            randomgames(){
+                const randomnumber = Math.random();
+                if (randomnumber > 0.5){
+                    return 'Played the witcher 3 but did not finish';
+                }else{
+                    return 'Completed Ratchet and Clank';
+                }
+            }
         }
     },
 );
