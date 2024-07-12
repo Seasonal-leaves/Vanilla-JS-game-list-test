bfabada9790dbc711cce76f8aba3d3e918ab013f
Rituparno Sen <ritam_sen@ymail.com>
2024-07-13 00:45:54 +0530

    added data objects to methods

--- creating and connecting vue app instances/app.js
+++ creating and connecting vue app instances/app.js
@@ -2,7 +2,9 @@ const app = Vue.createApp(
     {//The 2nd bracket is needed to configure various property of the Vue app
         data(){// here we have to name this data as it is not upto us, it is the way vue is coded. Here this means the value stored in data is a function
         return {   //this function used in data should always return an object. Not a number, string or anything else always an OBJECT
-        addedGame: 'Finsih the game and add it', //inside the return object we can add any key value pair as we like
+        addedGame: 'Finish the game and add it', //inside the return object we can add any key value pair as we like
+        addgame:'Played the witcher 3 but did not finish',
+
         intervuelink: 'https://joyful-speculoos-f3dc31.netlify.app/'
     }
         },
@@ -10,9 +12,8 @@ const app = Vue.createApp(
             randomgames(){
                 const randomnumber = Math.random();
                 if (randomnumber > 0.5){
-                    return 'Played the witcher 3 but did not finish';
-                }else{
-                    return 'Completed Ratchet and Clank';
+                    return       this.addedGame          }else{
+                    return this.addgame;
                 }
             }
         }
