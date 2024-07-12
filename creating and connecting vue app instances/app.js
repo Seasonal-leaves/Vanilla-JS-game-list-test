f77bf7a994bdb7340cf05d2b46a1ff7ab197df68
Rituparno Sen <ritam_sen@ymail.com>
2024-07-13 00:50:54 +0530

    added v-html tag for visual changes

--- creating and connecting vue app instances/app.js
+++ creating and connecting vue app instances/app.js
@@ -2,7 +2,7 @@ const app = Vue.createApp(
     {//The 2nd bracket is needed to configure various property of the Vue app
         data(){// here we have to name this data as it is not upto us, it is the way vue is coded. Here this means the value stored in data is a function
         return {   //this function used in data should always return an object. Not a number, string or anything else always an OBJECT
-        addedGame: 'Finish the game and add it', //inside the return object we can add any key value pair as we like
+        addedGame: '<h2>Finish the game and add it</h2>', //inside the return object we can add any key value pair as we like
         addgame:'Played the witcher 3 but did not finish',
 
         intervuelink: 'https://joyful-speculoos-f3dc31.netlify.app/'
